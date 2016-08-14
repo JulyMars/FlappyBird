c46662106e5e0692c4e56f13e66f907515344cfb
王雄 <wxwangxiong@gmail.com>
2016-08-14 09:38:09 +0800

    Add: learn flappy bird

---
+++ Assets/Scripts/Bird.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Bird : MonoBehaviour
+{
+
+	private float offsetX = 0.33333f;
+	private int frameCount = 0;
+	public float frameRate = 1/10f;
+	private float timer;
+
+	// Use this for initialization
+	void Start ()
+	{
+
+	}
+
+	// Update is called once per frame
+	void Update ()
+	{
+		timer += Time.deltaTime;
+		if (timer >= frameRate) {
+			timer -= frameRate;
+			frameCount++;
+			this.gameObject.GetComponent<Renderer> ().material.SetTextureOffset ("_MainTex", new Vector2 (frameCount % 3 * offsetX, 0));
+		}
+	}
+}
